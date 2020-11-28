0a828762fbc5129f2e5e109e0900f7c525841231
Adeeb Ahmad <adeebahmad0010@gmail.com>
2020-11-28 09:02:05 +0500

    Hello World

--- src/components/Home/Welfare.jsx
+++ src/components/Home/Welfare.jsx
@@ -1,27 +1,37 @@
 import React from "react";
 import Zoom from 'react-reveal/Zoom';
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.min.css";
+import SwiperCore, { Navigation } from 'swiper';
 
+SwiperCore.use([Navigation])
 const Welfare = () => {
   return (
     <div>
       {window.innerWidth > 786 ?
+    <Swiper loop={true} navigation={true}>
+    <SwiperSlide>
     <section className="bg_welfare hide_on_med">
-      <div className="container text-center text-white">
-        <h1 className="font-weight-bold">
-          Youth Education, Welfare & Employability
-        </h1>
-        <p className="w-50 mx-auto">
-          YEC is an initiative to empower youth with access to information,
-          opportunity & tools for Holistic Personal Development.
-        </p>
-        <div className="mt-5">
-          <button className="btn btn-theme btn-outline-theme btn-warning text-white py-2 px-5">
-            Learn More
-          </button>
-        </div>
+    <div className="container text-center text-white">
+      <h1 className="font-weight-bold">
+        Youth Education, Welfare & Employability
+      </h1>
+      <p className="w-50 mx-auto">
+        YEC is an initiative to empower youth with access to information,
+        opportunity & tools for Holistic Personal Development.
+      </p>
+      <div className="mt-5">
+        <button className="btn btn-theme btn-outline-theme btn-warning text-white py-2 px-5">
+          Learn More
+        </button>
       </div>
-    </section>
+    </div>
+  </section>
+    </SwiperSlide>
+  </Swiper>
     :
+    <Swiper loop={true} navigation={true}>
+      <SwiperSlide>
     <section className="col-12 bg_welfare">
     <Zoom>
       <div className="container display-4 text-center text-white">
@@ -40,6 +50,8 @@ const Welfare = () => {
       </div>
       </Zoom>
     </section>
+    </SwiperSlide>
+    </Swiper>
       }
     </div>
   );
